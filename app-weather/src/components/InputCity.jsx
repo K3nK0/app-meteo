03a2ba6d97b74b3d68be5df1ef28dc4241c16aaa
K3nK0 <uncommonmen@hotmail.fr>
2023-09-28 18:15:43 +0200

    component inputCity

--- app-weather/src/components/InputCity.jsx
+++ app-weather/src/components/InputCity.jsx
@@ -1,23 +1,60 @@
 import React, { useState } from 'react'
+import {nanoid} from "nanoid"
 
 export default function InputCity({location, setLocation}) {
 
-  const [getCity, setGetCity] = useState(location.city)
+  const [getCity, setGetCity] = useState("")
+
+  const [proposalsCity, setProposalsCity] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    setLocation({...location, city: getCity})
+    setLocation({
+      city: proposalsCity[0].name,
+      lat: proposalsCity[0].latitude,
+      long: proposalsCity[0].longitude
+    })
+
+    setGetCity("")
+    setProposalsCity([])
+  }
+
+  const handleInputLocation = async (citySearch) => {
+    try {
+      const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${citySearch}&count=10&language=fr&format=json`)
+      if(!response.ok) return
+      const data = await response.json()
+      const proposals = data.results
+      if(proposals !== undefined) setProposalsCity(proposals)
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <form onSubmit={handleSubmit} >
      <input 
         type="text"
-        className='w-full p-2.5 bg-white/10 rounded-lg shadow-xl'
+        list='proposals-city'
+        className='w-full p-2.5 text-white text-center text-4xl bg-white/10 rounded-lg shadow-xl placeholder:text-white placeholder:text-4xl'
+        placeholder={location.city}
         value={getCity}
-        onChange={e => setGetCity(e.target.value)}
-     /> 
+        onChange={e => {
+          setGetCity(e.target.value)
+          handleInputLocation(e.target.value)
+        }}
+     />
+     <datalist id='proposals-city'>
+      {proposalsCity.map(city => {
+        return <option 
+          key={nanoid(4)} 
+          value={city.name}
+          label={city.country}
+          />
+      })}
+      
+     </datalist>
     </form>
   )
 }
