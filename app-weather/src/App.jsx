fbc7e5b566f77ae9f1b68ee66365271caaa92419
K3nK0 <uncommonmen@hotmail.fr>
2023-10-01 22:36:09 +0200

    correction function apiOpenMeteo

--- app-weather/src/App.jsx
+++ app-weather/src/App.jsx
@@ -12,14 +12,17 @@ import WeatherPerHours from "./components/WeatherPerHours"
 
 function App() {
 
-  // getApiWeather()
+  let currentWeather = undefined
+  let dailyWeather = undefined
+  let allSchedules = undefined
+  const listHoursPerDay = []
+  let hoursPerDay = []
+
   const [location, setLocation] = useState({
     city: "",
     lat: "",
     long: ""
   })
-
-  // console.log("location", location);
 
   useEffect(() => {
     async function fetchData() {
@@ -29,64 +32,94 @@ function App() {
     fetchData();
   }, []);
 
-  const dataWeather = dataJSON
 
-  const currentWeather = dataWeather.current_weather
-  const dailyWeather = dataWeather.daily
-  const allSchedules = dataWeather.hourly
+  const [dataWeather, setDataWeather] = useState(null)
+  const [loading, setLoading] = useState(true)
+  console.log("dataWeather", dataWeather);
 
-  const listHoursPerDay = []
-  let hoursPerDay = []
+  useEffect(() => {
+    if (location.lat && location.long) {
+        async function fetchWeatherData() {
+          try {
+            await getApiWeather(location.lat, location.long, setDataWeather);
+            setLoading(false)
+          } catch (err) {
+            console.error("Error fetching weather data: ", err);
+            setLoading(false)
+          }
+      }
+      fetchWeatherData()
+    }
+  },[location])
 
-
-  for(let i = 0; i < allSchedules.time.length; i++){
+  if (loading) {
+    return <p>Loading...</p>
+  }
   
-    if(hoursPerDay[0] === undefined){
-      hoursPerDay.push(
-        {"time":allSchedules.time[i], 
-        "temp":allSchedules.temperature_2m[i], 
-        "humidity":allSchedules.relativehumidity_2m[i], 
-        "weathercode":allSchedules.weathercode[i],
-        "windSpeed": allSchedules.windspeed_10m[i],
-        "windDirection": allSchedules.winddirection_10m[i]
-      }
-      )
-    } 
-    else if(new Date(hoursPerDay[0].time).getDay() === new Date(allSchedules.time[i]).getDay()){
-      hoursPerDay.push(
-        {"time":allSchedules.time[i], 
-        "temp":allSchedules.temperature_2m[i], 
-        "humidity":allSchedules.relativehumidity_2m[i], 
-        "weathercode":allSchedules.weathercode[i],
-        "windSpeed": allSchedules.windspeed_10m[i],
-        "windDirection": allSchedules.winddirection_10m[i]
-      }
-      )
-      if(i + 1 === allSchedules.time.length){
+  if(dataWeather !== null && dataWeather !== undefined){
+    currentWeather = dataWeather.current_weather
+    dailyWeather = dataWeather.daily
+    allSchedules = dataWeather.hourly
+
+    
+
+
+    for(let i = 0; i < allSchedules.time.length; i++){
+    
+      if(hoursPerDay[0] === undefined){
+        hoursPerDay.push(
+          {"time":allSchedules.time[i], 
+          "temp":allSchedules.temperature_2m[i], 
+          "humidity":allSchedules.relativehumidity_2m[i], 
+          "weathercode":allSchedules.weathercode[i],
+          "windSpeed": allSchedules.windspeed_10m[i],
+          "windDirection": allSchedules.winddirection_10m[i]
+        }
+        )
+      } 
+      else if(new Date(hoursPerDay[0].time).getDay() === new Date(allSchedules.time[i]).getDay()){
+        hoursPerDay.push(
+          {"time":allSchedules.time[i], 
+          "temp":allSchedules.temperature_2m[i], 
+          "humidity":allSchedules.relativehumidity_2m[i], 
+          "weathercode":allSchedules.weathercode[i],
+          "windSpeed": allSchedules.windspeed_10m[i],
+          "windDirection": allSchedules.winddirection_10m[i]
+        }
+        )
+        if(i + 1 === allSchedules.time.length){
+          listHoursPerDay.push(hoursPerDay)
+          hoursPerDay = []
+        }
+      } else {
         listHoursPerDay.push(hoursPerDay)
         hoursPerDay = []
+        hoursPerDay.push(
+          {"time":allSchedules.time[i], 
+          "temp":allSchedules.temperature_2m[i], 
+          "humidity":allSchedules.relativehumidity_2m[i], 
+          "weathercode":allSchedules.weathercode[i],
+          "windSpeed": allSchedules.windspeed_10m[i],
+          "windDirection": allSchedules.winddirection_10m[i]
+        })
       }
-    } else {
-      listHoursPerDay.push(hoursPerDay)
-      hoursPerDay = []
-      hoursPerDay.push(
-        {"time":allSchedules.time[i], 
-        "temp":allSchedules.temperature_2m[i], 
-        "humidity":allSchedules.relativehumidity_2m[i], 
-        "weathercode":allSchedules.weathercode[i],
-        "windSpeed": allSchedules.windspeed_10m[i],
-        "windDirection": allSchedules.winddirection_10m[i]
-      })
     }
   }
+
 
   return (
     <>
       <div className="flex flex-col gap-5 min-w-[320px] w-screen max-w-[550px] mx-auto px-5 py-12 bg-gradient-to-b from-sky-500 to-indigo-500">
         <InputCity location={location} setLocation={setLocation} />
-        <CurrentWeather currentWeather={currentWeather} />
-        <TodaysWeather dailyWeather={dailyWeather} allSchedules={allSchedules} />
-        {listHoursPerDay.length >= 1 && <WeatherPerHours listHoursPerDay={listHoursPerDay} /> }
+        {dataWeather !== null ? (
+          <div>
+            <CurrentWeather currentWeather={currentWeather} />
+            <TodaysWeather dailyWeather={dailyWeather} allSchedules={allSchedules} />
+            {listHoursPerDay.length >= 1 && <WeatherPerHours listHoursPerDay={listHoursPerDay} /> }
+          </div>
+        ) : 
+        <p>Loading...</p>
+        }
       </div>
     </>
   )
--- app-weather/src/services/apiOpenMeteo.js
+++ app-weather/src/services/apiOpenMeteo.js
@@ -1,6 +1,18 @@
-export default function getApiWeather() {
+export default async function getApiWeather(lat, long, setDataWeather) {
+
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FBerlin`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+    .then(res => {
+        if(!res.ok){
+            throw new Error(`Fetch error: ${res.status}`)
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        setDataWeather(data)
+    })
+    .catch(err => {
+        console.log(err)
+        throw err
+    })
 }
